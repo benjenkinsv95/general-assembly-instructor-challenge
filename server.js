69562ea501d142f5eb1061f99f190134ac5555ce
Ben Jenkins <benjenkinsv95@gmail.com>
2018-08-08 14:47:20 -0400

    Cleaning up names in server.js
    Going to take a break (If GA people are seeing this and assuming I worked nonstop from when the repo was created.)
    
    20 mins invested so far.

--- server.js
+++ server.js
@@ -1,32 +1,41 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var path = require('path');
-var bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+// Load the favorite movies from favorited-movies.json
 app.get('/favorites', function(req, res) {
-    var data = fs.readFileSync('./data.json');
+    const data = fs.readFileSync('./favorited-movies.json');
     res.setHeader('Content-Type', 'application/json');
     res.send(data);
 });
 
-app.get('favorites', function(req, res){
+// Save a new favorited movie
+app.post('/favorites', function(req, res){
   if(!req.body.name || !req.body.oid) {
       res.send("Error");
-      return
-
-      var data = JSON.parse(fs.readFileSync('./data.json'));
-      data.push(req.body);
-      fs.writeFile('./data.json', JSON.stringify(data));
-      res.setHeader('Content-Type', 'application/json');
-      res.send(data);
+      return;
   }
+
+  // Load favorites
+  const favoritedMovies = JSON.parse(fs.readFileSync('./favorited-movies.json'));
+
+  // Add new favorite to existing favorites
+  favoritedMovies.push(req.body);
+
+  // Save new favorited movies
+  fs.writeFile('./favorited-movies.json', JSON.stringify(favoritedMovies));
+
+  // Return the updated favorited moviews
+  res.setHeader('Content-Type', 'application/json');
+  res.send(favoritedMovies);
 });
 
 app.listen(3000, function(){
